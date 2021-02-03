f92df6b21ff3aecc7c2fb4d10a50ffc3d91b21ab
Craig McFarland <craigtmcfarland@gmail.com>
2021-02-03 10:07:06 -0500

    finished

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var craigsAccount = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+            craigsAccount.Deposit(amountToDeposit);
+
+      
+
+            Console.WriteLine("How much would you like to withdraw?");
+
+            var amountToWithdraw = double.Parse(Console.ReadLine());
+            craigsAccount.Withdraw(amountToWithdraw);
+
+            Console.WriteLine($" Thank You Your balance is now {craigsAccount.GetBalance()}");
+
+
+
+        }
+    }
+}
